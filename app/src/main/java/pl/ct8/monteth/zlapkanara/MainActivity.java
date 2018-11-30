2220679dbebc8f2f9ae7bdfadf62da18b622a034
dudy <adam97dudek@gmail.com>
2018-11-30 20:48:43 +0100

    nie dziala

--- app/src/main/java/pl/ct8/monteth/zlapkanara/MainActivity.java
+++ app/src/main/java/pl/ct8/monteth/zlapkanara/MainActivity.java
@@ -2,12 +2,26 @@ package pl.ct8.monteth.zlapkanara;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import pl.ct8.monteth.zlapkanara.adapter.MyAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity  extends AppCompatActivity {
+    RecyclerView mBusLanes;
+    List<String> mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mList = new ArrayList();
+        mList.add("linia1");
+        mList.add("Linia2");
+        mList.add("Linia3");
+        mBusLanes = findViewById(R.id.rv_bus_lines);
+        MyAdapter adapter = new MyAdapter(mList);
+        mBusLanes.setAdapter(adapter);
     }
 }
--- app/src/main/java/pl/ct8/monteth/zlapkanara/adapter/MyAdapter.java
+++ app/src/main/java/pl/ct8/monteth/zlapkanara/adapter/MyAdapter.java
@@ -5,28 +5,44 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import pl.ct8.monteth.zlapkanara.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyAdapter extends RecyclerView.Adapter <>{
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+
     List<String> busLines = new ArrayList<>();
+    public MyAdapter(List<String> lanes){
+        busLines=lanes;
+
+    }
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(this).inflate(R.layout.item_bus_line, viewGroup, false);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_bus_line, viewGroup, false);
 
-        return new RecyclerView.ViewHolder(view);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
-
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        viewHolder.mLine.setText("Linia testowa");
     }
+
 
     @Override
     public int getItemCount() {
         return busLines.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        final TextView mLine;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mLine = itemView.findViewById(R.id.tv_line);
+        }
     }
 }
