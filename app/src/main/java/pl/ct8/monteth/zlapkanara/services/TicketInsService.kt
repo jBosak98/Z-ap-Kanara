0588ba2c9b565a382100004a992480b4132d4545
Konrad Bosak <greezlie@gmail.com>
2018-11-30 20:08:02 +0100

    TicketInsService template

---
+++ app/src/main/java/pl/ct8/monteth/zlapkanara/services/TicketInsService.kt
@@ -0,0 +1,12 @@
+package pl.ct8.monteth.zlapkanara.services
+
+import java.util.*
+
+class TicketInsService{
+    var lastCheckedData:Date = Calendar.getInstance().time
+    fun checkUpdates;
+    fun getTodaysData;
+    fun getWeekData;
+    private fun scrapData;
+
+}
