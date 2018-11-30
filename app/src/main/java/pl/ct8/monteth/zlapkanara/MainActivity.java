3742656f7a459d5365d6f18d188b28134595378f
dudy <adam97dudek@gmail.com>
2018-11-30 20:02:20 +0100

    RecyclerView and item add

--- app/src/main/java/pl/ct8/monteth/zlapkanara/MainActivity.java
+++ app/src/main/java/pl/ct8/monteth/zlapkanara/MainActivity.java
@@ -2,12 +2,16 @@ package pl.ct8.monteth.zlapkanara;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity  extends AppCompatActivity {
+    RecyclerView busLanes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        busLanes = findViewById(R.id.rv_bus_lanes);
+
     }
 }
