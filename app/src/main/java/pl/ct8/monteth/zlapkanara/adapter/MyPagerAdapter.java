ef10104a1a3b82d8e87267a5d7d0bce0805c66d9
dudy <adam97dudek@gmail.com>
2018-12-01 00:51:07 +0100

    2 fragment

--- app/src/main/java/pl/ct8/monteth/zlapkanara/adapter/MyPagerAdapter.java
+++ app/src/main/java/pl/ct8/monteth/zlapkanara/adapter/MyPagerAdapter.java
@@ -1,9 +1,13 @@
 package pl.ct8.monteth.zlapkanara.adapter;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
+import android.widget.Toast;
 import pl.ct8.monteth.zlapkanara.fragment.MainFragment;
+import pl.ct8.monteth.zlapkanara.fragment.WeekFragment;
 
 public class MyPagerAdapter extends FragmentPagerAdapter {
     private static int NUM_ITEMS = 2;
@@ -16,7 +20,8 @@ public class MyPagerAdapter extends FragmentPagerAdapter {
             case 0:
                 return MainFragment.newInstance();
             case 1:
-                return MainFragment.newInstance();
+                Log.i("fragment2", "fragment2");
+                return WeekFragment.newInstance();
             default:
                     return null;
         }
--- app/src/main/java/pl/ct8/monteth/zlapkanara/fragment/WeekFragment.java
+++ app/src/main/java/pl/ct8/monteth/zlapkanara/fragment/WeekFragment.java
@@ -2,14 +2,18 @@ package pl.ct8.monteth.zlapkanara.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import pl.ct8.monteth.zlapkanara.R;
 import pl.ct8.monteth.zlapkanara.adapter.MyAdapter;
+import pl.ct8.monteth.zlapkanara.adapter.MyWeekAdapter;
+import pl.ct8.monteth.zlapkanara.data.Day;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +24,8 @@ public class WeekFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     RecyclerView mBusLanes;
     TextView tvData;
-    List<String> mList;
-
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView tvStreet;
+    TextView tvRoutes;
 
 
 
@@ -48,13 +48,24 @@ public class WeekFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_week, container, false);
-        mList = new ArrayList();
-        tvData = view.findViewById(R.id.tv_day);
-        mList.add("linia1");
-        mList.add("Linia2");
-        mList.add("Linia3");
-        mBusLanes = view.findViewById(R.id.rv_bus_lines);
-        MyAdapter adapter = new MyAdapter(mList);
+        mBusLanes = view.findViewById(R.id.rv_bus_lines_week_fragment);
+        tvData = view.findViewById(R.id.tv_date);
+        tvStreet = view.findViewById(R.id.tv_street_week_fragment);
+        tvRoutes = view.findViewById(R.id.tv_routes);
+        mBusLanes.addItemDecoration(new DividerItemDecoration(getContext(),
+                DividerItemDecoration.VERTICAL));
+        List<String> routes = new ArrayList<>();
+        routes.add("123");
+        routes.add("1");
+        routes.add("K");
+        routes.add("910P");
+        List<Day> days = new ArrayList<>();
+
+        days.add(new Day("30.11", "ul. Grunwaldzka", routes));
+        days.add(new Day("30.11", "ul. Grunwaldzka", routes));
+        days.add(new Day("30.11", "ul. Grunwaldzka", routes));
+        days.add(new Day("30.11", "ul. Grunwaldzka", routes));
+        MyWeekAdapter adapter = new MyWeekAdapter(days);
         mBusLanes.setAdapter(adapter);
         mBusLanes.setLayoutManager(new LinearLayoutManager(view.getContext()));
         return view;
